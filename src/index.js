91b8a5436d35f271e99de9865c4b95aaf2fd6e9f
Coolkid <196774065@qq.com>
2024-06-23 13:11:55 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.coolkid.asia": dockerHub,
+  "quay.coolkid.asia": "https://quay.io",
+  "gcr.coolkid.asia": "https://gcr.io",
+  "k8s-gcr.coolkid.asia": "https://k8s.gcr.io",
+  "k8s.coolkid.asia": "https://registry.k8s.io",
+  "ghcr.coolkid.asia": "https://ghcr.io",
+  "cloudsmith.coolkid.asia": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.coolkid.asia": dockerHub,
 };
 
 function routeByHosts(host) {
